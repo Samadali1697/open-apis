2101790fd89302ee7afe13465adf438b42fd3098
Samad Ali Shah <samadali1697@gmail.com>
2020-04-26 19:58:00 +0200

    Added validator (#2)
    * validator
    
    * validator added
    
    Co-authored-by: Samad Ali Shah <samadali@Samads-MBP.local>

--- src/Application/LoginService.php
+++ src/Application/LoginService.php
@@ -4,15 +4,33 @@ declare(strict_types=1);
 namespace App\Application;
 
 use App\Infraustructure\Dto\UserDto;
+use App\Infraustructure\Validators\UserValidator;
 
 class LoginService {
+
+  /**
+   * @var UserValidator
+   */
+  private $validator;
+
+  /**
+   * @param UserValidator $validator .
+   */
+  public function __construct(UserValidator $validator) {
+    $this->validator = $validator;
+  }
 
   /**
   * @param UserDto $userDto .
   *
   * @return string
-  **/
+  */
   public function login(UserDto $userDto): string {
-      return 'Hi '. $userDto->getUsername(). ' pass: '. $userDto->getPassword();
+    $userValidatorData = $this->validator->validate($userDto);
+    if (!empty($userValidatorData)) {
+      return 'Invalid input: ' . $userValidatorData;
+    }
+
+    return 'Hi '. $userDto->getUsername(). ' pass: '. $userDto->getPassword();
   }
 }
---
+++ src/Infraustructure/Validators/UserValidator.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infraustructure\Validators;
+
+use App\Infraustructure\Dto\UserDto;
+
+class UserValidator {
+
+  /**
+  * @param UserDto $userDto .
+  *
+  * @return String|null
+  **/
+  public function validate(UserDto $userDto): ?String {
+      if ($userDto === null) {
+          return 'No form data';
+      }
+
+      if (empty($userDto->getUsername()) && empty($userDto->getPassword())) {
+          return 'Username and Password.';
+      }
+
+      if (empty($userDto->getUsername())) {
+          return 'Username.';
+      }
+
+      if (empty($userDto->getPassword())) {
+          return 'Password.';
+      }
+
+      return null;
+  }
+}
