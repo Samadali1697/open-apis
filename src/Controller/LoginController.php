d21398c33356ba2b18502fcdaaecfae784c70248
Samad Ali Shah <samadali1697@gmail.com>
2020-04-26 16:21:46 +0200

    add project struct
    Co-authored-by: Samad Ali Shah <samadali1697@gmail.com>

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,14 +1,37 @@
 <?php
+declare(strict_types=1);
+
 namespace App\Controller;
 
+use App\Application\LoginService;
+use App\Infraustructure\Dto\UserDto;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 class LoginController {
-  public function login(Request $request)
+  private const USER_NAME = 'username';
+  private const PASSWORD = 'password';
+
+  private $loginService = null;
+
+  public function __construct()
+  {
+
+  }
+
+  /**
+  * @param Request $request .
+  * @return Response
+  **/
+  public function login(Request $request): Response
   {
     //$name = $request->query->get('name');
-    $name = json_decode($request->getContent(), true);
-    return new Response('Hi '. $name['name']);
+    $username = json_decode($request->getContent(), true)[self::USER_NAME];
+    $password = json_decode($request->getContent(), true)[self::PASSWORD];
+
+    $loginService = new LoginService();
+    $response = $loginService->login(new UserDto($username, $password));
+
+    return new Response($response);
   }
 }
---
+++ src/Infraustructure/Dto/UserDto.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infraustructure\Dto;
+
+class UserDto {
+  /**
+  * @var string
+  **/
+  private $username;
+
+  /**
+  * @var string
+  **/
+  private $password;
+
+  /**
+  * @param string $username .
+  * @param string $password  .
+  **/
+  public function __construct(string $username, string $password) {
+      $this->username = $username;
+      $this->password = $password;
+  }
+
+  /**
+  * @return string
+  **/
+  public function getUsername(): string {
+    return $this->username;
+  }
+
+  /**
+  * @return string
+  **/
+  public function getPassword(): string {
+    return $this->password;
+  }
+}
